a88206c430be5e2fddb9ee85222dbef635208dc1
Manh Pham <manhpham90vn@icloud.com>
2023-12-04 16:56:01 +0700

    update

--- 20-the-preprocessor.c
+++ 20-the-preprocessor.c
@@ -3,15 +3,7 @@
 #define PI 3.14
 #define sum(x, y) (x + y)
 
-const int DEBUG = 1;
-
 int main(void) {
-    #if DEBUG == 1
-        printf("DEBUG");
-    #else
-        printf("NOT DEBUG");
-    #endif
-
     printf("%d", sum(1, 2));
 
     #ifdef PI
